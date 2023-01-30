2166a301e17ecae247d344cf660d50b83cb07527
olive-su <1466su@naver.com>
2023-01-30 11:36:20 +0900

    feat:수경_테스트2

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 // 수경 테스트
+// 아니다 위에 코드 무시해주세요
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
